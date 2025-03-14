54fdf114c59502e6d4ad6b8b44f4662b7f6a5219
God is graceful <godisgraceful@op.pl>
2025-03-14 16:45:55 +0100

    I change the command (contact)

--- commands/contact.py
+++ commands/contact.py
@@ -1,14 +1,16 @@
-import discord
-from discord.ext import commands
+# import discord
+# from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = commands.Bot(command_prefix='!', intents=intents)
+# intents = discord.Intents.default()
+# intents.message_content = True
+# client = commands.Bot(command_prefix='!', intents=intents)
 
-@client.tree.command(name="contact", description="Kontakt do autora bota")
-async def contact(interaction: discord.Integration):
-    embed = discord.Embed(
-        title="Kontakt",
-        description="Jeśli chcesz zgłosić błąd lub dać propozycję zmian w bocie skontaktuj się ze mną:\n\nDiscord: **code_joan**\nInstagram: **god.is.graceful**",
-        color=12370112)
-    await interaction.response.send_message(embed=embed)
+# @client.tree.command(name="contact", description="Kontakt do autora bota")
+# async def contact(interaction: discord.Integration):
+#     embed = discord.Embed(
+#         title="Kontakt",
+#         description="Jeśli chcesz zgłosić błąd lub dać propozycję zmian w bocie skontaktuj się ze mną:\n\nDiscord: **code_joan**\nInstagram: **god.is.graceful**",
+#         color=12370112)
+#     await interaction.response.send_message(embed=embed)
+
+I change the command
