58af33043c02bd35b6f464cee38b7b870da57302
God is graceful <godisgraceful@op.pl>
2025-03-17 18:03:11 +0100

    Update class PaginatorView

--- commands/help.py
+++ commands/help.py
@@ -8,51 +8,37 @@ intents.message_content = True
 client = commands.Bot(command_prefix='!', intents=intents)
 
 class PaginatorView(discord.ui.View):
-    def __init__(
-        self, 
-        embeds:List[discord.Embed]
-    ) -> None:
+    def __init__(self, embeds: List[discord.Embed]) -> None:
         super().__init__(timeout=None)
-        self._embeds = embeds
-        self._queue = deque(embeds)
-        self._initial = embeds[0]
-        self._current_page = 1
-        self._len = len(embeds)
+        self.embeds = deque(embeds)
+        self.current_page = 1
+        self.total_pages = len(embeds)
 
-        if self._len == 1:
+        if self.total_pages == 1:
             self.previous_page.disabled = True
             self.next_page.disabled = True
 
-    def get_page_number(self) -> str:
-        return f"Strona {self._current_page} z {self._len}"
+    def update_footer(self) -> discord.Embed:
+        embed = self.embeds[0]
+        embed.set_footer(text=f"Strona {self.current_page} z {self.total_pages}")
+        return embed
+
+    async def change_page(self, interaction: discord.Interaction, direction: int):
+        self.embeds.rotate(direction)
+        self.current_page = (self.current_page - direction - 1) % self.total_pages + 1
+        await interaction.response.edit_message(embed=self.update_footer())
 
     @discord.ui.button(style=discord.ButtonStyle.secondary, emoji="⬅️")
     async def previous_page(self, interaction: discord.Interaction, _):
-        self._queue.rotate(1)
-        embed = self._queue[0]
-        if self._current_page > 1:
-            self._current_page -= 1
-        else:
-            self._current_page = self._len
-        embed.set_footer(text=self.get_page_number())
-        await interaction.response.edit_message(embed=embed)
+        await self.change_page(interaction, 1)
 
     @discord.ui.button(style=discord.ButtonStyle.secondary, emoji="➡️")
     async def next_page(self, interaction: discord.Interaction, _):
-        self._queue.rotate(-1)
-        if self._current_page < self._len:
-            self._current_page += 1
-        else:
-            self._current_page = 1
-        embed = self._queue[0]
-        embed.set_footer(text=self.get_page_number())
-        await interaction.response.edit_message(embed=embed)
+        await self.change_page(interaction, -1)
 
     @property
     def initial(self) -> discord.Embed:
-        embed = self._initial
-        embed.set_footer(text=self.get_page_number())
-        return embed
+        return self.update_footer()
 
 @client.tree.command(name="help", description="Pomoc")
 async def help(interaction: discord.Interaction):
